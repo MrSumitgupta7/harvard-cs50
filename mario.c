8c097cffd982edd94138cb0f03bc95b14de80280
Sumit Gupta <165358836+MrSumitgupta7@users.noreply.github.com>
2024-06-01 03:29:40 +0530

    voila we have learned regarding constant and nested for loops .

---
+++ mario.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+    for (int i = 0 ;i < 4; i++)
+
+    {
+        printf("?");
+    }
+
+    printf("\n");
+}
+
+{
+    constant int n = 5;
+    for ( int i =0; i<n; i++)
+
+    {
+       for ( int j =0; j<n; j++)
+       {
+        printf("#");
+       }
+
+       printf("\n");
+
+    } 
+}
