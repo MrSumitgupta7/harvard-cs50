f0a5a40c2ad99646e3d0f890584137b9ccb8404b
Sumit Gupta <165358836+MrSumitgupta7@users.noreply.github.com>
2024-06-03 04:16:26 +0530

    so this file nothing but a scribble book for revison lecture

---
+++ week1(coding scribble).c
@@ -0,0 +1,34 @@
+int call = 3;
+
+int country_code = 65;
+
+/*
+shortcut or assignment operators
+*/
+
+
+int calls =;
+
+calls = calls +2;
+calls = calls -2;
+calls = calls * 2;
+calls = calls / 2; 
+
+/**
+input and printing
+*/
+
+/* using cs50.h
+*/
+
+int calls = get_int("Calls: ");
+printf("calls is %i\n", calls);
+
+/**
+
+types and format codes*/
+
+int(%i)
+float(%f)
+char(%c)
+string($s)
