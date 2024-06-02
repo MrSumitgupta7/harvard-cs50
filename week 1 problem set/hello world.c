05bc2906b252407552efac4cdc8147f1976a0573
Sumit Gupta <165358836+MrSumitgupta7@users.noreply.github.com>
2024-06-03 04:32:43 +0530

    Now in this updated code we are including first name and last name as first label and last label

--- week 1 problem set/hello world.c
+++ week 1 problem set/hello world.c
@@ -2,7 +2,11 @@
 #include<cs50.h>
 int main  (void)
 {
-string label = get_string ("What is your name?");
-printf("Hello, %s\n, label");
+
+string first_label = get_string("What is your first  name?");
+
+string second_label = get_string("What is your last name ?");
+
+printf("Hello, %s %s\n, first_label, last_label");
 
 }
