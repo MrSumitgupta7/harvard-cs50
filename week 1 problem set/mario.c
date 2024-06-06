d887e93571454dde57dea496e95ce7e1eabe3c34
Sumit Gupta <165358836+MrSumitgupta7@users.noreply.github.com>
2024-06-06 05:55:05 +0530

    Inspirng from Mario Game we are printing left side pyramid style hashes

---
+++ week 1 problem set/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int lenght);
+
+int main (void)
+
+{
+    int height = get_int("Height: ");
+    print_row(height);
+}
+
+void print_row(int length)
+
+{
+
+for (int i =0; i < length; i++)
+{
+    
+    printf('#');
+}
+
+}
